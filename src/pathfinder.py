514a25bf6afd73d7fd687eeb8e28cd2dc84f2c8b
DaRealBurnz <miniheroes24@gmail.com>
2023-07-30 11:49:15 +1000

    pathfind test 3

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -10,7 +10,7 @@ def pathfind(enemy, pos, objDict, center):
         if obj["type"] == 6:
             if (obj["position"][0][0] >= pos[0]-100 or obj["position"][1][1] >= pos[1]-100 or obj["position"][2][0] <= pos[0]+100 or obj["position"][3][1] <= pos[1]+100):
                 return "path", center
-    if enemyDist >= 300 and not checkLOS(enemy["position"], pos, objDict):
+    if enemyDist >= 300 or (not checkLOS(enemy["position"], pos, objDict)):
         return "path", enemy["position"]
     return "move", -1
         
